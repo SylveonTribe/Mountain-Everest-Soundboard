5fe55850874b3bf6db13c3f03eeb45b1c518b5a9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-07 19:11:07 +0100

    Projektdateien hinzufügen.

---
+++ Mountain Everest Soundboard/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Mountain_Everest_Soundboard
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.DPB1Path = new System.Windows.Forms.TextBox();
+      this.DPB1Select = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.button3 = new System.Windows.Forms.Button();
+      this.DPB2Select = new System.Windows.Forms.Button();
+      this.DPB2Path = new System.Windows.Forms.TextBox();
+      this.DPB3Select = new System.Windows.Forms.Button();
+      this.DPB3Path = new System.Windows.Forms.TextBox();
+      this.DPB4Select = new System.Windows.Forms.Button();
+      this.DPB4Path = new System.Windows.Forms.TextBox();
+      this.SelectSound = new System.Windows.Forms.OpenFileDialog();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(9, 7);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(209, 24);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Display Button 1 Sound:";
+      // 
+      // DPB1Path
+      // 
+      this.DPB1Path.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB1Path.Location = new System.Drawing.Point(13, 33);
+      this.DPB1Path.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB1Path.Name = "DPB1Path";
+      this.DPB1Path.ReadOnly = true;
+      this.DPB1Path.Size = new System.Drawing.Size(162, 29);
+      this.DPB1Path.TabIndex = 1;
+      // 
+      // DPB1Select
+      // 
+      this.DPB1Select.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB1Select.Location = new System.Drawing.Point(179, 33);
+      this.DPB1Select.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB1Select.Name = "DPB1Select";
+      this.DPB1Select.Size = new System.Drawing.Size(39, 29);
+      this.DPB1Select.TabIndex = 2;
+      this.DPB1Select.Text = "...";
+      this.DPB1Select.UseVisualStyleBackColor = true;
+      this.DPB1Select.Click += new System.EventHandler(this.DPB1Select_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(237, 7);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(209, 24);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Display Button 2 Sound:";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label3.Location = new System.Drawing.Point(693, 7);
+      this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(209, 24);
+      this.label3.TabIndex = 9;
+      this.label3.Text = "Display Button 4 Sound:";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label4.Location = new System.Drawing.Point(465, 7);
+      this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(209, 24);
+      this.label4.TabIndex = 6;
+      this.label4.Text = "Display Button 3 Sound:";
+      // 
+      // button3
+      // 
+      this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3.Location = new System.Drawing.Point(918, 33);
+      this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(69, 29);
+      this.button3.TabIndex = 12;
+      this.button3.Text = "Save";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // DPB2Select
+      // 
+      this.DPB2Select.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB2Select.Location = new System.Drawing.Point(407, 34);
+      this.DPB2Select.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB2Select.Name = "DPB2Select";
+      this.DPB2Select.Size = new System.Drawing.Size(39, 29);
+      this.DPB2Select.TabIndex = 14;
+      this.DPB2Select.Text = "...";
+      this.DPB2Select.UseVisualStyleBackColor = true;
+      this.DPB2Select.Click += new System.EventHandler(this.DPB2Select_Click);
+      // 
+      // DPB2Path
+      // 
+      this.DPB2Path.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB2Path.Location = new System.Drawing.Point(238, 33);
+      this.DPB2Path.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB2Path.Name = "DPB2Path";
+      this.DPB2Path.ReadOnly = true;
+      this.DPB2Path.Size = new System.Drawing.Size(165, 29);
+      this.DPB2Path.TabIndex = 13;
+      // 
+      // DPB3Select
+      // 
+      this.DPB3Select.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB3Select.Location = new System.Drawing.Point(635, 34);
+      this.DPB3Select.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB3Select.Name = "DPB3Select";
+      this.DPB3Select.Size = new System.Drawing.Size(39, 29);
+      this.DPB3Select.TabIndex = 16;
+      this.DPB3Select.Text = "...";
+      this.DPB3Select.UseVisualStyleBackColor = true;
+      this.DPB3Select.Click += new System.EventHandler(this.DPB3Select_Click);
+      // 
+      // DPB3Path
+      // 
+      this.DPB3Path.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB3Path.Location = new System.Drawing.Point(463, 33);
+      this.DPB3Path.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB3Path.Name = "DPB3Path";
+      this.DPB3Path.ReadOnly = true;
+      this.DPB3Path.Size = new System.Drawing.Size(168, 29);
+      this.DPB3Path.TabIndex = 15;
+      // 
+      // DPB4Select
+      // 
+      this.DPB4Select.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB4Select.Location = new System.Drawing.Point(863, 33);
+      this.DPB4Select.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB4Select.Name = "DPB4Select";
+      this.DPB4Select.Size = new System.Drawing.Size(39, 29);
+      this.DPB4Select.TabIndex = 18;
+      this.DPB4Select.Text = "...";
+      this.DPB4Select.UseVisualStyleBackColor = true;
+      this.DPB4Select.Click += new System.EventHandler(this.DPB4Select_Click);
+      // 
+      // DPB4Path
+      // 
+      this.DPB4Path.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DPB4Path.Location = new System.Drawing.Point(697, 32);
+      this.DPB4Path.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.DPB4Path.Name = "DPB4Path";
+      this.DPB4Path.ReadOnly = true;
+      this.DPB4Path.Size = new System.Drawing.Size(162, 29);
+      this.DPB4Path.TabIndex = 17;
+      // 
+      // SelectSound
+      // 
+      this.SelectSound.Filter = "Music-Files|*.wav";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(993, 70);
+      this.Controls.Add(this.DPB4Select);
+      this.Controls.Add(this.DPB4Path);
+      this.Controls.Add(this.DPB3Select);
+      this.Controls.Add(this.DPB3Path);
+      this.Controls.Add(this.DPB2Select);
+      this.Controls.Add(this.DPB2Path);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.DPB1Select);
+      this.Controls.Add(this.DPB1Path);
+      this.Controls.Add(this.label1);
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.Name = "Form1";
+      this.ShowInTaskbar = false;
+      this.Text = "Mountain Everest Sounboard";
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox DPB1Path;
+    private System.Windows.Forms.Button DPB1Select;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button DPB2Select;
+    private System.Windows.Forms.TextBox DPB2Path;
+    private System.Windows.Forms.Button DPB3Select;
+    private System.Windows.Forms.TextBox DPB3Path;
+    private System.Windows.Forms.Button DPB4Select;
+    private System.Windows.Forms.TextBox DPB4Path;
+    private System.Windows.Forms.OpenFileDialog SelectSound;
+  }
+}
+
---
+++ Mountain Everest Soundboard/Form1.cs
@@ -0,0 +1,199 @@
+﻿using Mountain_Everest_Soundboard.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Mountain_Everest_Soundboard
+{
+  public partial class Form1 : Form
+  {
+    [DllImport("user32.dll")]
+    public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+    [DllImport("user32.dll")]
+    public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+    private NotifyIcon trayicon;
+    
+    Key key1;
+    Key key2;
+    Key key3;
+    Key key4;
+    string configpath = Path.Combine(Application.StartupPath, "keys.conf");
+    string soundpath1 = string.Empty;
+    string soundpath2 = string.Empty;
+    string soundpath3 = string.Empty;
+    string soundpath4 = string.Empty;
+    bool isRunning = true;
+    public Form1()
+    {
+      InitializeComponent();
+      trayicon = new NotifyIcon()
+      {
+        Icon = this.Icon,
+        ContextMenu = new ContextMenu(new MenuItem[] {
+              new MenuItem("Show", ShowForm),
+              new MenuItem("Exit", Exit)
+        }),
+        Visible = true
+      };
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      ReadConfigFile();
+
+      Thread Keyb = new Thread(KeyboardCheck);
+      Keyb.SetApartmentState(ApartmentState.STA);
+      Keyb.Start();
+    }
+
+    private void ReadConfigFile()
+    {
+      if (File.Exists(configpath))
+      {
+        foreach (string line in File.ReadAllLines(configpath))
+        {
+          if (line.StartsWith("Key1path="))
+          {
+            soundpath1 = line.Substring(line.IndexOf("=") + 2);
+            DPB1Path.Text = soundpath1;
+          }
+          if (line.StartsWith("Key2path="))
+          {
+            soundpath2 = line.Substring(line.IndexOf("=") + 2);
+            DPB2Path.Text = soundpath2;
+          }
+          if (line.StartsWith("Key3path="))
+          {
+            soundpath3 = line.Substring(line.IndexOf("=") + 2);
+          }
+          if (line.StartsWith("Key4path="))
+          {
+            soundpath4 = line.Substring(line.IndexOf("=") + 2);
+          }
+        }
+      }
+
+      key1 = Key.F21;
+      key2 = Key.F22;
+      key3 = Key.F23;
+      key4 = Key.F24;
+    }
+
+    private void KeyboardCheck()
+    {
+      while (isRunning)
+      {
+        Thread.Sleep(40);
+        if ((Keyboard.GetKeyStates(key1) & KeyStates.Down) > 0 && !String.IsNullOrEmpty(soundpath1))
+        {
+          System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundpath1);
+          player.Play();
+        }
+        if ((Keyboard.GetKeyStates(key2) & KeyStates.Down) > 0 && !String.IsNullOrEmpty(soundpath2))
+        {
+          System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundpath2);
+          player.Play();
+        }
+        if ((Keyboard.GetKeyStates(key3) & KeyStates.Down) > 0 && !String.IsNullOrEmpty(soundpath3))
+        {
+          System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundpath3);
+          player.Play();
+        }
+        if ((Keyboard.GetKeyStates(key4) & KeyStates.Down) > 0 && !String.IsNullOrEmpty(soundpath4))
+        {
+          System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundpath4);
+          player.Play();
+        }
+      }
+    }
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+    {
+      isRunning = false;
+    }
+
+    private void DPB1Select_Click(object sender, EventArgs e)
+    {
+      SelectSound.FileName = DPB1Path.Text;
+      SelectSound.ShowDialog();
+      soundpath1 = SelectSound.FileName;
+      DPB1Path.Text = SelectSound.FileName;
+    }
+
+    private void DPB2Select_Click(object sender, EventArgs e)
+    {
+      SelectSound.FileName = DPB2Path.Text;
+      SelectSound.ShowDialog();
+      soundpath2 = SelectSound.FileName;
+      DPB2Path.Text = SelectSound.FileName;
+    }
+
+    private void DPB3Select_Click(object sender, EventArgs e)
+    {
+      SelectSound.FileName = DPB3Path.Text;
+      SelectSound.ShowDialog();
+      soundpath3 = SelectSound.FileName;
+      DPB3Path.Text = SelectSound.FileName;
+    }
+
+    private void DPB4Select_Click(object sender, EventArgs e)
+    {
+      SelectSound.FileName = DPB4Path.Text;
+      SelectSound.ShowDialog();
+      soundpath4 = SelectSound.FileName;
+      DPB4Path.Text = SelectSound.FileName;
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      WriteConfig();
+    }
+
+    private void WriteConfig()
+    {
+      if (File.Exists(configpath))
+      {
+        File.Delete(configpath);
+      }
+
+      FileStream stream = new FileStream(configpath, FileMode.OpenOrCreate);
+      using (StreamWriter writer = new StreamWriter(stream))
+      {
+        writer.WriteLine("Key1path= " + DPB1Path.Text);
+        writer.WriteLine("Key2path= " + DPB2Path.Text);
+        writer.WriteLine("Key3path= " + DPB3Path.Text);
+        writer.WriteLine("Key4path= " + DPB4Path.Text);
+      }
+    }
+
+    private void ShowForm(object sender, EventArgs e)
+    {
+      Show();
+      this.WindowState = FormWindowState.Normal;
+    }
+
+    private void Exit(object sender, EventArgs e)
+    {
+      isRunning = false;
+      Application.Exit();
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      if (this.WindowState == FormWindowState.Minimized)
+      {
+        Hide();
+      }
+    }
+  }
+}
